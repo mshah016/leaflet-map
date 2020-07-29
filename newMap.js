62e6f397d446c51e8e73302e559b48899707448a
Meera Shah <meera.shah96@gmail.com>
2020-07-28 22:39:39 -0700

    added legend

--- newMap.js
+++ newMap.js
@@ -103,7 +103,31 @@ var streetmap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}
     }
   }).addTo(earthquakes);
 
-  
+
+  var legend = L.control({position: 'bottomright'});
+
+  legend.onAdd = function (map) {
+
+      var div = L.DomUtil.create('div', 'info legend'),
+      grades = [1, 2, 3, 4, 5],
+      labels = [];
+
+      // loop through our density intervals and generate a label with a colored square for each interval
+      for (var i = 0; i < grades.length; i++) {
+          div.innerHTML +=
+              '<i style="background:' + getColor(grades[i] + 1) + '"></i> ' +
+              grades[i] + (grades[i + 1] ? '&ndash;' + grades[i + 1] + '<br>' : '+');
+  }
+
+  return div;
+  };
+
+  legend.addTo(myMap);
 });
+
+
+
+
+
 
 
